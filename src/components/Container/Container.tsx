dc3e469089c3c56781be106440bfc6d06be49727
Mathéo Lopes <matheo.lps91@gmail.com>
2025-01-12 11:31:17 +0100

    Refactor application structure: remove App.css, integrate Container and AdvancedSearch components, and update App.tsx for improved layout and functionality

---
+++ src/components/Container/Container.tsx
@@ -0,0 +1,8 @@
+
+import React from 'react';
+
+export default function Container( { children }: { children: React.ReactNode } ) {
+  return (
+    <div className=' max-w-4xl mx-auto'>{children}</div>
+  )
+}
